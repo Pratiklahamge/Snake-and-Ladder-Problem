0927639396808322efb4413d3c0b4b51a11c618d
Pratik Lahamge <89896925+Pratiklahamge@users.noreply.github.com>
2021-12-09 11:29:15 +0530

    Added: Option Check

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
+
 		int position = 0;
 		int dieRoll = 1 + (int) Math.floor(Math.random() * 10) % 6;
 		System.out.println("Die Rolled:" + dieRoll);
